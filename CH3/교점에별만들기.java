f9d0fff7cbb2969eaee7d6860f6632cf0e32278e
Hanseung2 <121085516+Hanseung2@users.noreply.github.com>
2023-08-14 15:51:25 +0900

    Update 교점에별만들기.java

--- CH3/교점에별만들기.java
+++ CH3/교점에별만들기.java
@@ -12,7 +12,7 @@ public class Solution {
         }
     }
 
-    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
+    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {    //직선의 교점을 찾는 메서드
         double x = (double) (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
         double y = (double) (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1);
 
@@ -21,7 +21,7 @@ public class Solution {
         return new Point((long) x, (long) y);
     }
 
-    private Point getMinimumPoint(List<Point> points) {
+    private Point getMinimumPoint(List<Point> points) {    //최소인 x, y 좌표를 찾는 메서드
         long x = Long.MAX_VALUE;
         long y = Long.MAX_VALUE;
 
@@ -33,7 +33,7 @@ public class Solution {
         return new Point(x, y);
     }
 
-    private Point getMaximumPoint(List<Point> points) {
+    private Point getMaximumPoint(List<Point> points) {    //최대인 x, y 좌표를 찾는 메서드
         long x = Long.MIN_VALUE;
         long y = Long.MIN_VALUE;
 
@@ -59,7 +59,7 @@ public class Solution {
         Point minimum = getMinimumPoint(points);
         Point maximum = getMaximumPoint(points);
 
-        int width = (int) (maximum.x - minimum.x + 1);
+        int width = (int) (maximum.x - minimum.x + 1);    //교점을 나타날때의 사각형을 최소로 만들어준다.
         int height = (int) (maximum.y - minimum.y + 1);
 
         char[][] arr = new char[height][width];
@@ -67,7 +67,7 @@ public class Solution {
             Arrays.fill(row, '.');
         }
 
-        for (Point p : points) {
+        for (Point p : points) {    //교점의 좌표와 이차원 배열의 좌표가 일치하지 않으므로, 사각형을 만들었을때 왼쪽 열과 제일 위쪽 행의 값이 0이 되기 때문에 x는 최소인 x를 빼주고 y는 최대인 y에서 빼준다.
             int x = (int) (p.x - minimum.x);
             int y = (int) (maximum.y - p.y);
             arr[y][x] = '*';
